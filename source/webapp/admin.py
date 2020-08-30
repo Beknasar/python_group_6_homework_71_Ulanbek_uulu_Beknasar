a8b44f30b8f7afcdb3c64de5a82b16e093511751
Beknasar <680633@gmail.com>
2020-08-30 13:46:06 +0600

    Модели данных зарегистрированы в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from webapp.models import Category, Product
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'amount', 'price']
+    list_display_links = ('pk', 'name')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
