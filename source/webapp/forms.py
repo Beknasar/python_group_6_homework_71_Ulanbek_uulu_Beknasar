83decd58b3c42923554a0266b084d59d2766d0b8
Beknasar <680633@gmail.com>
2020-08-30 13:57:48 +0600

    Добавлена главная страница с пагинацией. Добавлены стили.

---
+++ source/webapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Product
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Найти")
+
+#
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = ['name', 'description', 'category', 'amount', 'price']
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Product
 
-# Create your views here.
+from django.views.generic import ListView
+from django.http import HttpResponseNotAllowed
+from .forms import SearchForm
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'products'
+    paginate_by = 5
+    paginate_orphans = 0
+    ordering = ['category', 'name']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        kwargs['form'] = form
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
+        data = Product.objects.all()
+
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(Q(name__icontains=search) | Q(description__icontains=search))
+        return data
