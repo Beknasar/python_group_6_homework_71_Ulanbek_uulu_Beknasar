d517f3b8f1a7e0627c0ed95545a76a7dd3c7012c
Beknasar <680633@gmail.com>
2020-10-21 23:36:40 +0600

    Создано приложение api_v1.

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'widget_tweaks',
     'accounts',
-    'webapp'
+    'webapp',
+    'api_v1'
 ]
 
 MIDDLEWARE = [
